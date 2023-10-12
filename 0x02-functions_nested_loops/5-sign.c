364e8dd065623ee994ef09312c8d499f422c7929
MOHAMED-FAWZY2025 <f2525awzy@gmail.com>
2023-10-11 19:12:20 -0700

    task_6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- * print_sign - print-sign-num
- * @y: the int to check
- * Return: 1 and prints + if y is greater than zero
- * 0 and prints 0 if y is zero
- * -1 and prints - if y is less than zero
+ * print_sign - print -sign-num
+ * @n: int to be checked
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
-int print_sign(int y)
+int print_sign(int n)
 {
-	if (y > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
-	} else if( y == 0 )
-}
+	} else if (n == 0)
+	{
 		_putchar(48);
 		return (0);
-	} else if(y < 0)
+	} else if (n < 0)
 	{
 		_putchar('-');
 	}
