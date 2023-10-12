e1e56c6613e66eab223f67516945e90f2a4eaab2
MOHAMED-FAWZY2025 <f2525awzy@gmail.com>
2023-10-11 17:58:16 -0700

    task_6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute value_int
+ * @y: tint to be checked
+ * Return: absolute value of int
+ */
+int _abs(int y)
+{
+	if (y >= 0)
+	{
+		return (y);
+	}
+	return (-y);
+}
