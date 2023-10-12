b6748b16aa52d84c696803e9ff7b2e41cc44dd34
MOHAMED-FAWZY2025 <f2525awzy@gmail.com>
2023-10-11 19:14:55 -0700

    task_8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * jack_bauer - write every minute-day of Jack Bauer
+ * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
  */
 void jack_bauer(void)
 {
-	int i, y;
+	int i, j;
 
 	i = 0;
 
 	while (i < 24)
 	{
-		y = 0;
-		while (y < 60)
+		j = 0;
+		while (j < 60)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((y / 10) + '0');
-			_putchar((y % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
-			y++;
+			j++;
 		}
 		i++;
 	}
