884288fa0b11c7006bf622e6c41e96c8b13dfa19
MOHAMED-FAWZY2025 <f2525awzy@gmail.com>
2023-10-11 18:24:34 -0700

    task_10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - add two int then return result
+ * @a: integer to be add to b
+ * @b: integer to be add to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
