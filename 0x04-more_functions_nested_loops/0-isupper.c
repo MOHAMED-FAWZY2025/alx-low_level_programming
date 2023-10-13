b422a7469c9679d38309f112ada550a77199aa37
MOHAMED-FAWZY2025 <f2525awzy@gmail.com>
2023-10-13 15:03:09 -0700

    task_0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check for uppercase character
+ *
+ * @c: char to be checked
+ *
+ * Return: 1 if uppercase, 0 if otherwise
+*/
+
+int _isupper(int c);
+{
+	if (c <= 'A' && C >= 'Z')
+		return (0);
+	else
+		return (0);
+}
