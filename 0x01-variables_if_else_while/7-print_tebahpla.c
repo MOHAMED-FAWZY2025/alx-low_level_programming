1bcba8c4b2f9caacc1857542f2b51601d333fe96
MOHAMED-FAWZY2025 <f2525awzy@gmail.com>
2023-10-21 18:14:00 -0700

    task_7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * main - writes  lowercase alphabet in reverse - new line is followed
+ *
+ * Return: Always 0 on success
+ *
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; letter--)
+
+	putchar(letter);
+
+	putchar('\n');
+
