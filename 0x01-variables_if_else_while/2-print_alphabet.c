bd28d2e3f595667735d14630468184450d184ce7
MOHAMED-FAWZY2025 <f2525awzy@gmail.com>
2023-10-21 17:31:52 -0700

    task_2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -  entrry point
+ *
+ * Description: prints the alphabet in lowercase
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+		putchar(\n);
+return (0);
+}
